509bf4e701962855f209d033b3c2885dd97ee1ff
rutrob <faherlele@gmail.com>
2024-07-27 11:43:50 +0200

    Initialize project

---
+++ src/main/java/rutrob/task/RecruitmentTaskAtiperaApplication.java
@@ -0,0 +1,13 @@
+package rutrob.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskAtiperaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentTaskAtiperaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rutrob/task/RecruitmentTaskAtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package rutrob.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskAtiperaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
